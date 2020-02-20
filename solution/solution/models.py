272425c5e8ff71eef2fa89d6c8b87ae24da7b4c1
Nir Geller <gnir.work@gmail.com>
2020-02-20 21:04:23 +0200

    Finished parser

--- solution/solution/models.py
+++ solution/solution/models.py
@@ -1,24 +1,41 @@
+from __future__ import annotations
 from typing import List
+
 
 class Book:
     def __init__(self, book_id: int, score: int):
         self.id = book_id
         self.score = score
-        self.library_ids: List[int] = []
 
-    def add_library(self, library_id: int):
-        """
-        Add library to book
-        """
-        self.library_ids.append(library_id)
+    def __str__(self):
+        return f"book id: {self.id} book_score: {self.score}"
+
+    @classmethod
+    def parse_from_line(cls, line: str) -> List[Book]:
+        return [cls(book_id, int(book_score)) for book_id, book_score in enumerate(line.split())]
     
 class Library:
-    def __init__(self, library_id: int, books: List[Book], registration_time: int, throughput: int):
+    def __init__(self, library_id: int, registration_time: int, throughput: int):
         self.id = library_id
-        self.books = books
+        self.books: List[Book] = []
         self.registration_time = registration_time
         self.throughput = throughput
 
+    @property
+    def number_of_books(self):
+        return len(self.books)
+
+    def add_books(self, books: List[Book]):
+        self.books = books
+
+    def __str__(self):
+        books_string = list(map(str, self.books))
+        return f"books: {books_string} registration_time:{self.registration_time} throughput:{self.throughput}"
+
+    @classmethod
+    def parse_from_lines(cls, library_id: int, library_definition: str) -> Library:
+        _, registration_time, throughput = map(int, library_definition.split())
+        return cls(library_id, registration_time, throughput)
 
 class RoundConfig:
     def __init__(self, books_number: int, libraries_number: int, days_number: int):
@@ -29,10 +46,12 @@ class RoundConfig:
         self.books: List[Book] = []
 
     def __str__(self):
-        return f"books number: {self.books_number} libraries number: {self.libraries_number} days: {self.days_number}"
+        books_string = list(map(str, self.books))
+        library_string = list(map(str, self.libraries))
+        return f"books number: {self.books_number} libraries number: {self.libraries_number} days: {self.days_number}\nbooks: {books_string}\nlibraries_string: {library_string}"
 
     @classmethod
-    def parse_from_line(cls, line: str):
+    def parse_from_line(cls, line: str) -> RoundConfig:
         """
         Create he RoundConfig from a data line.
         """
--- solution/solution/parser.py
+++ solution/solution/parser.py
@@ -1,5 +1,5 @@
 FILE_NAME = "data.txt"
-from .models import RoundConfig, Book, Library
+from models import RoundConfig, Book, Library
 
 def get_file_lines(path_to_file: str):
     with open(path_to_file) as data_file:
@@ -8,9 +8,17 @@ def get_file_lines(path_to_file: str):
 
 def parse_file(path_to_file: str):
     data_lines = get_file_lines(path_to_file)
-    round_config = RoundConfig.parse_from_line(next(data_lines))
-
-
+    round_config: RoundConfig = RoundConfig.parse_from_line(next(data_lines))
+    books = Book.parse_from_line(next(data_lines))
+    round_config.books = books
+    for library_id in range(round_config.libraries_number):
+        library_data = next(data_lines)
+        library_books = next(data_lines)
+        library = Library.parse_from_lines(library_id, library_data)
+        library_books_ids = map(int, library_books.split())
+        library.add_books([round_config.books[book_id] for book_id in library_books_ids])
+        round_config.libraries.append(library)
+         
     return round_config
 
 if __name__ == "__main__":
